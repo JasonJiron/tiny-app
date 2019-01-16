497fa2a6c976fbb932529be8c758d45ae1236fc1
Jason <jason.jiron@gmail.com>
2019-01-16 00:28:32 +0000

    added urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -26,10 +26,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars)
 })
 
-// app.get("/urls/:id", (req, res) => {
-//   let templateVars = { shortURL: req.params.id }
-//   res.render("urls_show", templateVars)
-// })
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { 
+    shortURL: req.params.id,
+    urls: urlDatabase
+   } 
+  res.render("urls_show", templateVars)
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
