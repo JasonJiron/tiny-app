fdecd4830525fcdbcf9965953383f81e88084a48
Jason <jason.jiron@gmail.com>
2019-01-16 02:13:41 +0000

    added more ejs files & random UID generator function

--- express_server.js
+++ express_server.js
@@ -1,10 +1,13 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
-var urlDatabase = {
+
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -26,6 +29,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars)
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new")
+})
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { 
     shortURL: req.params.id,
@@ -33,6 +40,16 @@ app.get("/urls/:id", (req, res) => {
    } 
   res.render("urls_show", templateVars)
 })
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+})
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
